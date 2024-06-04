16d68798204801e1ee9ba357c21cd82a549a146c
Hiep1210 <nguyenhoanghiep1210003@gmail.com>
2024-06-05 00:26:56 +0700

    m

---
+++ app/src/main/java/com/example/widgetstudy/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.widgetstudy;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "Blazed.ttf");
+        textView.setTypeface(font);
+    }
+}
